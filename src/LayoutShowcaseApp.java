543d6bd33baae81a9c3c66efac3be1bfa935b76e
Brian Zakariya <zakariyabrian@gmail.com>
2026-04-08 02:40:21 +0700

    answer question number 2, and create the structure

---
+++ src/LayoutShowcaseApp.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Brian
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LayoutShowcaseApp extends JFrame implements ActionListener {
+
+    // CardLayout and container
+    CardLayout cardLayout;
+    JPanel cardPanel;
+
+    // two main panel
+    JPanel panelA;
+    JPanel panelB;
+
+    // button switch
+    JButton btnSwitchA;
+    JButton btnSwitchB;
+
+    public LayoutShowcaseApp() {
+        
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public static void main(String[] args) {
+        new LayoutShowcaseApp();
+    }
+}
